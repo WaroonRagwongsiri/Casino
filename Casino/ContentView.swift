cc8cc8e3c145258ad86e9592d1b938d51d9c64a9
admin <st10682@kjn.ac.th>
2024-06-15 20:40:25 -0700

    remove unneccessary

--- Casino/ContentView.swift
+++ Casino/ContentView.swift
@@ -6,11 +6,8 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    
     var body: some View {
         VStack(content: {
             Text(verbatim: "")
